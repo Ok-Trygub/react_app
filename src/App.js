14fe1a26189abbf5a295df554f8b028b0342a257
Oksana Trygub <ok.trygub@gmail.com>
2022-05-30 22:24:26 +0300

    add card2_component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Card from './components/Card'
+import Card2 from './components/Card2'
 
 function App() {
   return (
       <div className="App">
         <Card />
+
+          <Card2 title = 'hi' />
+          <Card2 text = 'how are you?'/>
+          <Card2 title = 'hi' text = 'how are you?'/>
       </div>
   );
 
