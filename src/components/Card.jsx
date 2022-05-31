e7ec4fdb8316f4fa2d8025b1b7ac09b1cd00a71d
Oksana Trygub <ok.trygub@gmail.com>
2022-05-31 19:00:30 +0300

    add function Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
+// function Card() {
+//     return (
+//         <div className="card">
+//             <div className="card-body">
+//                 <h4 className="card-title">Card title</h4>
+//                 <p className="card-text">Some quick example text to build on the card</p>
+//                 <button type={this.props.type} className="btn btn-primary">Go somewhere</button>
+//             </div>
+//         </div>
+//
+//     )
+// }
+
 class Card extends React.Component {
     render() {
         return (
